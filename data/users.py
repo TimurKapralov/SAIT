8fcf59c62ceffce4886390b344fee436d3951de5
arhairullin <azathrln@yandex.ru>
2024-04-01 18:50:15 +0300

    commitfafa

--- data/users.py
+++ data/users.py
@@ -22,7 +22,7 @@ class User(SqlAlchemyBase):
                                      default=datetime.datetime.now)
 
     def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
